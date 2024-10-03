6bb06f4cbb12bbd592007ac897b3b9eb035425a1
LLParis <sirlondonparis@gmail.com>
2024-10-02 22:43:39 -0700

    Work In Progress

--- src/Aggregation.java
+++ src/Aggregation.java
@@ -7,8 +7,123 @@ public class Aggregation {
     private Fraction fractionField;
     private Rectangle [] arrayField;
     private double doubleField;
-    
-   
+
+    /**
+     * No-argument constructor that initializes:
+     * fractionField to 0/1
+     * arrayField to an array with one Rectangle width width and length 1
+     * doubleField to 0.0
+     */
+   public Aggregation() {
+       this.fractionField = new Fraction(0,1); // Fraction initialized to 0/1
+       this.arrayField = new Rectangle[1]; // Array of 1 Rectangle
+       this.arrayField[0] = new Rectangle(1, 1); // Rectangle with width and length 1
+       this.doubleField = 0.0; // Double initialized to 0.0
+   }
+
+    /**
+     * Constructor that accepts a Fraction, an array of Rectangles, and a double value.
+     * Deep copies are made for the Fraction and the array of Rectangles.
+     *
+     * @param fraction The Fraction object to be deep copied
+     * @param array The array of Rectangle objects to be deep copied
+     * @param doubleField The double value to be set
+     */
+   public Aggregation(Fraction fraction, Rectangle[] array, double doubleField) {
+       // Deep copy of Fraction
+       this.fractionField = new Fraction(fraction);
+       // Deep copy of the Rectanlge array
+       this.arrayField = new Rectangle[array.length];
+       for (int i = 0; i < array.length; i++) {
+           this.arrayField[i] = new Rectangle(array[i]);
+       }
+       this.doubleField = doubleField;
+   }
+
+    /**
+     * Copy Constructor: Creates a new Aggregation object by deep copying another Aggregation object.
+     *
+     * @param other The Aggregation object to be deep copied
+     */
+   public Aggregation(Aggregation other) {
+       // Deep copy of Fraction
+       this.fractionField = new Fraction(other.fractionField);
+       // Deep copy of the Rectangle array
+       this.arrayField = new Rectangle[other.arrayField.length];
+       for (int i = 0; i < other.arrayField.length; i++) {
+           this.arrayField[i] = new Rectangle(other.arrayField[i]);
+       }
+       // Copy the double value
+       this.doubleField = other.doubleField;
+   }
+
+    /**
+     * Accessor method for fractionField.
+     * Returns a deep copy of the Fraction object.
+     *
+     * @return A deep copy of the fractionField
+     */
+   public Fraction getFractionField() {
+       return new Fraction(this.fractionField);
+   }
+
+    /**
+     * Accessor method for arrayField.
+     * Returns a deep copy of the Rectangle array.
+     *
+     * @return A deep copy of the arrayField
+     */
+   public Rectangle[] getArrayField() {
+       Rectangle[] arrayCopy = new Rectangle[this.arrayField.length];
+       for (int i =  0; i < this.arrayField.length; i++) {
+           arrayCopy[i] = new Rectangle(this.arrayField[i]);
+       }
+       return arrayCopy;
+   }
+
+    /**
+     * Accessor method for doubleField.
+     * Returns the double value directly (since it's a primitive type).
+     *
+     * @return
+     */
+   public double getDoubleField() {
+       return this.doubleField;
+   }
+
+    /**
+     * Mutator method for fractionField.
+     * Sets the fractionField by making a deep copy of the input Fraction.
+     *
+     * @param fraction The Fraction to set (deep copied)
+     */
+   public void setFractionField(Fraction fraction) {
+       this.fractionField = new Fraction(fraction);
+   }
+
+    /**
+     * Mutator method for arrayField.
+     * Sets the arrayField by making a deep copy of the input Rectangle array.
+     *
+     * @param array The Rectangle array to be set (deep copied)
+     */
+   public void setArrayField(Rectangle[] array) {
+       this.arrayField = new Rectangle[array.length];
+       for (int i = 0; i < array.length; i++) {
+           this.arrayField[i] = new Rectangle(array[i]);
+       }
+   }
+
+    /**
+     * Mutator method for doubleField.
+     * Sets the doubleField.
+     *
+     * @param doubleField The double value to set
+     */
+   public void setDoubleField(double doubleField) {
+       this.doubleField = doubleField;
+   }
+
     public String toString()
     {
         StringBuilder strB = new StringBuilder("Fraction: "+fractionField.toString());
@@ -24,7 +139,7 @@ public class Aggregation {
     }
     
     /**
-     * Mathod needed only to test for a deep copy
+     * Method needed only to test for a deep copy
      * @param a value to set an element with 
      */
     public void forDeepCopyTesting(int a)
--- src/Fraction.java
+++ src/Fraction.java
@@ -42,10 +42,10 @@ public class Fraction {
      * The sign of the fraction is determined by the whole part (wholeNum).
      * If the whole part is negative, the entire fraction will be negative. The fraction is simplified.
      *
-     * @param wholeNum the whole part of the mixed fraction
-     * @param numerator the numerator of the fraction
-     * @param denominator the denominator of the fraction
-     * @throws IllegalArgumentException if the denominator is zero or if the numerator or denominator is negative.
+     * @param wholeNum The whole part of the mixed fraction
+     * @param numerator The numerator of the fraction
+     * @param denominator The denominator of the fraction
+     * @throws IllegalArgumentException If the denominator is zero or if the numerator or denominator is negative.
      */
     public Fraction(int wholeNum, int numerator, int denominator) {
         if (denominator == 0) {
@@ -79,23 +79,35 @@ public class Fraction {
     public Fraction(Fraction obj) {
         this.numerator = obj.numerator;
         this.denominator = obj.denominator;
+        simplify(); // Simplify copied fraction
     }
 
     /**
-     Accessor methods
-     * @return
+     * Accessor method for the numerator.
+     *
+     * @return The numerator of the fraction
      */
     public int getNum() {
         return this.numerator;
     }
+
+    /**
+     * Accessor method for the denominator.
+     *
+     * @return The denominator of the fraction
+     */
     public int getDenom() {
         return this.denominator;
     }
 
     /**
-     Mutator method
-     * @param n
-     * @param d
+     * Mutator method: Sets both the numerator and denominator.
+     * If the denominator is zero, an IllegalArgumentException is thrown.
+     * The fraction is simplified after the values are set.
+     *
+     * @param n The new numerator
+     * @param d The new denominator
+     * @throws IllegalArgumentException If the denominator is zero
      */
     public void set(int n, int d) {
         if (d == 0) {
@@ -130,10 +142,10 @@ public class Fraction {
     }
 
     /**
-     * Calculate the greatest common divisor using Euclidean Algorithm
-     * @param a
-     * @param b
-     * @return
+     * Calculates the greatest common divisor of two numbers using the Euclidean Algorithm.
+     * @param a The first number
+     * @param b The second number
+     * @return The greatest common divisor of a and b
      */
     private int gcd(int a, int b) {
         if (b == 0) {
@@ -143,55 +155,80 @@ public class Fraction {
     }
 
     /**
-     * Addition method
-     * @param obj
-     * @return
+     * Addition method: Adds two fractions and returns the result as a new Fraction object.
+     * The fractions are first converted to have a common denominator, and numerators are adjusted.
+     * The Fraction is simplified.
+     *
+     * @param obj The Fraction to add
+     * @return A new Fraction representing the sum
      */
     public Fraction add(Fraction obj) {
+        // Find the common denominator
         int commonDenom = this.denominator * obj.denominator;
+        // Adjust the numerators and add
         int newNumerator = (this.numerator * obj.denominator) + (obj.numerator * this.denominator);
-
+        simplify();
+        // Return the new simplified Fraction
         return new Fraction(newNumerator, commonDenom);
     }
 
     /**
-     * Subtraction method
-     * @param obj
-     * @return
+     * Subtraction Method: Subtracts a fraction from another fraction and returns the result.
+     * The fractions are first converted to have a common denominator, and the numerators are adjusted.
+     * The resulting fraction is simplified.
+     *
+     * @param obj The Fraction to subtract from this fraction
+     * @return A new Fraction representing the difference between this fraction and obj
      */
     public Fraction subtract(Fraction obj) {
+        // Find the common denominator
         int commonDenom = this.denominator * obj.denominator;
+        // Adjust the numerators and subtract
         int newNumerator = (this.numerator * obj.denominator) - (obj.numerator * this.denominator);
-
+        simplify();
         return new Fraction(newNumerator, commonDenom);
     }
 
     /**
-     * Multiplication
-     * @param obj
-     * @return j
+     * Multiplies two fractions and returns the result as a new Fraction object.
+     * The numerators are multiplied together, and the denominators are multiplied together.
+     * The resulting fraction is simplified.
+     *
+     * @param obj The Fraction to multiply by
+     * @return A new Fraction representing the product of this fraction and obj
      */
     public Fraction multiply(Fraction obj) {
-        return new Fraction(this.numerator * obj.numerator, this.denominator * obj.denominator);
+        Fraction product = new Fraction(this.numerator * obj.numerator, this.denominator * obj.denominator);
+        product.simplify();
+        return product;
     }
 
     /**
-     * Division
-     * @param obj
-     * @return
+     * Divides two fractions and returns the result as a new Fraction object.
+     * The current fraction is divided by multiplying it by the reciprocal of obj.
+     * The resulting fraction is simplified.
+     *
+     * @param obj The Fraction to divide by
+     * @return A new Fraction representing the quotient
+     * @throws ArithmeticException If the numerator of obj is zero (cannot divide by zero)
      */
+
     public Fraction divide(Fraction obj) {
         if (obj.numerator == 0) {
             throw new ArithmeticException("Cannot divide by zero.");
         }
-        return new Fraction(this.numerator * obj.denominator, this.denominator * obj.numerator);
+
+        Fraction quotient = new Fraction(this.numerator * obj.denominator, this.denominator * obj.numerator);
+        quotient.simplify();
+        return quotient;
     }
 
 
     /**
-     * Less
-     * @param obj
-     * @return
+     * Less Method: Compares two fractions and returns true if this fraction is less than the parameter fraction.
+     * Both fractions are converted to have a common denominator before comparing their numerators.
+     * @param obj The Fraction to compare with
+     * @return True if this fraction is smaller than obj
      */
     public boolean less(Fraction obj) {
         int thisNumerator = this.numerator * obj.denominator;
@@ -201,9 +238,11 @@ public class Fraction {
     }
 
     /**
-     * More
-     * @param obj
-     * @return
+     * Compares two fractions and returns true if this fraction is greater than the parameter fraction.
+     * Both fractions are converted to have a common denominator before comparing their numerators.
+     *
+     * @param obj the Fraction to compare with
+     * @return true if this fraction is greater than obj
      */
     public boolean more(Fraction obj) {
         int newNumerator = this.numerator * obj.denominator;
@@ -213,9 +252,11 @@ public class Fraction {
     }
 
     /**
-     * Equals
-     * @param obj
-     * @return true if fractions are equal
+     * Equals Method: Compares two fractions and returns true if they are equal.
+     * Both fractions are converted to have a common denominator before comparing their numerators.
+     *
+     * @param obj The Fraction to compare with
+     * @return true if this fraction is equal to obj
      */
     public boolean equals(Fraction obj) {
         int newNumerator = this.numerator * obj.denominator;
@@ -226,11 +267,20 @@ public class Fraction {
         return newNumerator == otherNumerator;
     }
 
+    /**
+     * Converts the fraction into a string.
+     * Proper fractions are represented as numerator/denominator.
+     * Improper fractions are represented as numerator/denominator or wholePart and remainder/denominator.
+     * Zero fractions are represented as 0/1 or 0.
+     * For negative fractions, the negative sign is handled in both the improper and mixed form.
+     *
+     * @return A string representation of the fraction.
+     */
     @Override
     public String toString() {
         if (this.numerator == 0) {
             // Case for zero fraction (0/1 or 0)
-            return this.numerator + "/" + this.denominator + " or " + this.numerator;
+            return "0/1 or 0";
         } else if (Math.abs(this.numerator) < this.denominator) {
             // Case for proper fractions (numerator < denominator) 3/22
             return this.numerator + "/" + this.denominator;
